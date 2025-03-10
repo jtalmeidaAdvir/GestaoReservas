53571913084e79f5eb0dcfe78f16dedbe554b1bb
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-03-10 22:39:34 +0000

    com

--- backend/models/Bus.js
+++ backend/models/Bus.js
@@ -3,12 +3,14 @@ module.exports = (sequelize, DataTypes) => {
         id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
         nome: { type: DataTypes.STRING, allowNull: false },
         nlugares: { type: DataTypes.INTEGER, allowNull: false },
-        imagem: { type: DataTypes.BLOB("long"), allowNull: true }, // Alterado para armazenar dados binários
+        imagem: { type: DataTypes.BLOB("long"), allowNull: true },
         createdOn: { type: DataTypes.STRING, allowNull: true },
         createdBy: { type: DataTypes.STRING, allowNull: true },
         updatedOn: { type: DataTypes.STRING, allowNull: true },
         updatedBy: { type: DataTypes.STRING, allowNull: true },
         isActive: { type: DataTypes.BOOLEAN, defaultValue: true }
+    }, {
+        timestamps: false, // Desabilita os campos createdAt e updatedAt
     });
 
     return Bus;
