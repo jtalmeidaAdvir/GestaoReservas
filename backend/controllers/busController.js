52566c13da5d60c56dcb52b5fabcf64b51183ea9
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-03-10 21:47:47 +0000

    com

--- backend/controllers/busController.js
+++ backend/controllers/busController.js
@@ -9,6 +9,17 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 
+const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
+const formattedDate = formatDate(new Date());
 
 
 
@@ -30,8 +41,8 @@ exports.createBus = async (req, res) => {
             nlugares, 
             imagem, 
             createdBy: email, 
-            createdOn: new Date().toISOString(),
-            updatedOn: new Date().toISOString()
+            createdOn: formattedDate,
+            updatedOn: formattedDate
 
         });
 
